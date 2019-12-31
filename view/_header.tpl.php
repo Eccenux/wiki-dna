88822ba44ca6cd0685eb1b4a1a4e1c7df07475e9
Eccenux <eccenux+git@gmail.com>
2019-12-31 01:11:14 +0100

    html5 + mobile

--- view/_header.tpl.php
+++ view/_header.tpl.php
@@ -1,21 +1,19 @@
-<!DOCTYPE html 
-     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl">
+<!DOCTYPE html>
+<html lang="pl">
 <head>
 	<title><?=$strPageTitle?> <?php if (!empty($strDate2Check)) { echo $strDate2Check; } ?></title>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta charset="UTF-8">
 
-	<meta name="author" content="Maciej Jaros" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
 	<meta name="copyright" content="Maciej Jaros" />
 
-	<link rel="stylesheet" type="text/css" href="view/main.css?2327" />
+	<link rel="stylesheet" type="text/css" href="view/main.css?0055" />
 
 	<!-- time en(de)coding & Calendar -->
 	<link href="view/calend_t.css?0111" rel="stylesheet" type="text/css" />
 	<script type="text/javascript" src="./lib/calend_t.js?1337"></script>
 	<script type="text/javascript">
-	<!--
 	//
 	//  Kalendarz - ustawienia
 	//
@@ -23,7 +21,6 @@
 	ncalend.days_holders_bgcolor_mark = '#088';
 	ncalend.months = ["sty","lut","mar","kwi","maj","cze","lip","sie","wrz","pa&#378;","lis","gru"];
 	ncalend.err_parse_time = 'Nieprawid\u0142owy format czasu!\nSpr\u00F3buj 13:10, albo 14:57:29';
-	// -->
 	</script>
 </head>
 <body>
